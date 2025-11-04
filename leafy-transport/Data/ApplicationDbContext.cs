5772b21cbd7579e955af7ef2c9772f5bcecad0b9
WebSpruce <kacperswi12363@gmail.com>
2025-11-04 09:36:42 +0100

    init

---
+++ leafy-transport/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace leafy_transport.Data;
+
+public sealed class ApplicationDbContext 
+{
+    
+}
